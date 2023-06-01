a5fb9b2249ce1ec1b50016e761ffeae429aa6505
thepsySunny <tjsgnl2002@thepsyentist.com>
2023-06-01 11:19:43 +0900

    feat:laundry-complex생성 및 설명 문서 등록

--- apps/seroi/src/constant/laundry/index.ts
+++ apps/seroi/src/constant/laundry/index.ts
@@ -1,4 +1,1 @@
 export { default as SVCComplexAttributes } from './SVCComplexAttributes';
-export { default as SVCContainerAttributes } from './SVCContainerAttributes';
-export { default as SVCMessageAttributes } from './SVCMessageAttributes';
-export { default as SVCBoxAttributes } from './SVCBoxAttributes';
