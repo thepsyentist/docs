ae6224b84fd8d478bb0c459ae13d12c15e6c7fd2
thepsySunny <tjsgnl2002@thepsyentist.com>
2023-06-01 11:19:43 +0900

    feat:laundry-complex생성 및 설명 문서 등록

---
+++ apps/seroi/src/constant/laundry/SVCComplexAttributes.ts
@@ -0,0 +1,15 @@
+import { Attribute } from '@docs/ui/src/types/list.type';
+import attributes from '../attributes';
+
+const SVCComplexAttributes: Attribute[] = [
+  { ...attributes.type, description: '버킷의 Type입니다. "COMPLEX"로 고정 값 입니다.' },
+  attributes.id,
+  { name: 'name', required: false, type: 'string', description: '단지의 이름입니다. \n ex) 롯데캐슬스카이' },
+  attributes.deleted,
+  attributes.published,
+  attributes.updated,
+  attributes.created,
+  { name: 'userData.belong', required: false, type: '"seroi" | "citech" | "seroi2"', description: '함의 구분을 짓기 위해 사용합니다. \nseroi: 기본 새로이함 \ncitech: citech함 \nseroi2: 새로이 2분할 함(초창기 새로이함)' }
+];
+
+export default SVCComplexAttributes;
---
+++ apps/seroi/src/constant/laundry/index.ts
@@ -0,0 +1,1 @@
+export { default as SVCComplexAttributes } from './SVCComplexAttributes';
