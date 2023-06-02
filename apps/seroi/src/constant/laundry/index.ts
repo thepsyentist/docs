70f1af3399cf210dcdc18202ed3234c768aa7b4d
thepsySunny <tjsgnl2002@thepsyentist.com>
2023-06-02 17:36:53 +0900

    feat: Container 파일 생성 및 문서 설명 추가

--- apps/seroi/src/constant/laundry/index.ts
+++ apps/seroi/src/constant/laundry/index.ts
@@ -1,1 +1,4 @@
 export { default as SVCComplexAttributes } from './SVCComplexAttributes';
+export { default as SVCContainerAttributes } from './SVCContainerAttributes';
+export { default as SVCMessageAttributes } from './SVCMessageAttributes';
+export { default as SVCBoxAttributes } from './SVCBoxAttributes';
