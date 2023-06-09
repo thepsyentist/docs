0e9b2c5a33623a97048937dd1b32bd80c8eeca2c
thepsySunny <tjsgnl2002@thepsyentist.com>
2023-06-09 17:48:38 +0900

    feat: 문자정보 관련 문서 생성 및 작성

--- apps/seroi/src/constant/laundry/index.ts
+++ apps/seroi/src/constant/laundry/index.ts
@@ -1,3 +1,4 @@
 export { default as SVCComplexAttributes } from './SVCComplexAttributes';
 export { default as SVCContainerAttributes } from './SVCContainerAttributes';
 export { default as SVCOrderAttributes } from './SVCOrderAttributes';
+export { default as SVCMessageAttributes } from './SVCMessageAttributes';
