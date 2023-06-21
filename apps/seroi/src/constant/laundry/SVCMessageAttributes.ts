157b1bd45932f8c0650d06af62c6586013cbfed2
bytrustu <bytrustu@gmail.com>
2023-06-22 07:25:49 +0900

    refactor: 문구 개선

--- apps/seroi/src/constant/laundry/SVCMessageAttributes.ts
+++ apps/seroi/src/constant/laundry/SVCMessageAttributes.ts
@@ -4,12 +4,12 @@ import attributes from '../attributes';
 const SVCMessage: Attribute[] = [
   { ...attributes.type, description: 'Doc의 Type입니다. "MESSAGEINFO"로 고정 값 입니다.' },
   attributes.id,
-  { ...attributes.bucketId, description: '문자의 버킷 ID값 입니다. 고유 값 입니다.(SVCBucket_881050921521152)' },
+  { ...attributes.bucketId, description: '문자의 버킷 ID값 입니다. 고유 값 입니다. (SVCBucket_881050921521152)' },
   {
     name: 'title',
     required: true,
     type: 'string',
-    description: '문자 내용 앞에 들어갈 제목입니다. \n ex)[무인세탁함] TEST 세탁함에 세탁이 완료되었습니다. 의 문자 내용에서 "무인세탁함" 을 말합니다.',
+    description: '문자 내용 앞에 들어갈 제목입니다. \nex) [무인세탁함] TEST 세탁함에 세탁이 완료되었습니다. 의 문자 내용에서 "무인세탁함" 을 말합니다.',
   },
   attributes.deleted,
   attributes.published,
