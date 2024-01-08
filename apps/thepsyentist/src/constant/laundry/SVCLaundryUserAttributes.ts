25f280e64195edd282833744c311550a15988468
thepsySunny <tjsgnl2002@thepsyentist.com>
2024-01-08 15:58:23 +0900

    feat: 유저정보에적립금 정보 추가

--- apps/thepsyentist/src/constant/laundry/SVCLaundryUserAttributes.ts
+++ apps/thepsyentist/src/constant/laundry/SVCLaundryUserAttributes.ts
@@ -16,6 +16,7 @@ const SVCOrder: Attribute[] = [
 const userData: Attribute[] = [
   { name: 'containerId', required: false, type: 'string', description: '해당 유저가 고객인 경우 이용하는 함의 id값에 대한 정보입니다.' },
   { name: 'userUniqueId', required: false, type: 'string', description: '해당 유저가 고객인 경우 고객의 고유 Id입니다.' },
+  { name: 'points', required: false, type: 'number', description: '해당 유저가 고객인 경우 고객의 적립금입니다.' },
   { name: 'onComplexId', required: false, type: 'string[]', description: '해당 유저가 배송기사인 경우 맡고 있는 단지의 id값들에 대한 정보입니다.' },
   { name: 'onContainerIdList', required: false, type: 'string[]', description: '해당 유저가 배송기사인 경우 맡고 있는 보관함의 id값들에 대한 정보입니다.' },
 ];
