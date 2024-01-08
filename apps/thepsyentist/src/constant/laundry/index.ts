fa2c2ef486187fc083b93cd711e8e3401d178887
thepsySunny <tjsgnl2002@thepsyentist.com>
2024-01-08 15:50:48 +0900

    LAUNDRY_NOTIFY고지정보 타입정보추가

--- apps/thepsyentist/src/constant/laundry/index.ts
+++ apps/thepsyentist/src/constant/laundry/index.ts
@@ -8,3 +8,4 @@ export { default as SVCLaundryReceivedAttributes } from './SVCLaundryReceivedAtt
 export { default as SVCCounterAttributes } from './SVCCounterAttributes';
 export { default as SVCCommentAttributes } from './SVCCommentAttributes';
 export { default as SVCCategoryAttributes } from './SVCCategoryAttributes';
+export { default as SVCLaundryNotifyAttributes } from './SVCLaundryNotifyAttributes';
